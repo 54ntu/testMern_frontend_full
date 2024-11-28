1e5cc50c6d11d3274d598209935096ca2ecc52c0
54ntu <chaudharysantaram8@gmail.com>
2024-11-28 11:05:36 +0545

    toastprovider is setup

---
+++ src/Toastprovider.jsx
@@ -0,0 +1,12 @@
+import { Toaster } from "react-hot-toast";
+import React from "react";
+
+const Toastprovider = () => {
+  return (
+    <div>
+      <Toaster />
+    </div>
+  );
+};
+
+export default Toastprovider;
